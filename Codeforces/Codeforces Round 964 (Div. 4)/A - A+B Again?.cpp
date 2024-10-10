9afa001445694b059772b2d91fc16b81b905a3b1
LondonJackTheRipper <moriatyjohnwatson@gmail.com>
2024-10-10 23:08:25 +0700

    Create A - A+B Again?.cpp

---
+++ Codeforces/Codeforces Round 964 (Div. 4)/A - A+B Again?.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+
+  Mua Code nhu mua Florentino !!
+  
+  Tags:
+*/
+
+#define faster() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define pb push_back
+
+void input()
+{
+	string s; cin >> s;
+	
+	cout << (s[0] - '0') + (s[1] - '0') << '\n';
+}
+
+int main()
+{
+   faster();
+
+	int t; cin >> t;
+	while (t--) input();
+
+	return 0;
+}
