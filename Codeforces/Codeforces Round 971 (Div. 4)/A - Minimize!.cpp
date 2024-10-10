22748cc9edd62ba4d8b88df4b978c552c7d21314
LondonJackTheRipper <moriatyjohnwatson@gmail.com>
2024-10-10 23:00:09 +0700

    Create A - Minimize!.cpp

---
+++ Codeforces/Codeforces Round 971 (Div. 4)/A - Minimize!.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  https://codeforces.com/profile/quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define faster() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+
+signed main()
+{
+	if (fopen("test.inp", "r"))
+	{
+		freopen("test.inp", "r", stdin);
+		freopen("test.out", "w", stdout);
+	}
+	faster();
+
+	int t; cin >> t;
+	while (t--)
+	{
+		int a, b; cin >> a >> b;
+		
+		cout << -a + b << '\n';
+	}
+
+	return 0;
+}
