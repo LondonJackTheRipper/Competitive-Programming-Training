4426621d5bd2361b12bf7767fa8af0ff13253f9a
John Watson <moriatyjohnwatson@gmail.com>
2024-10-12 22:24:04 +0700

    Create A - Takahashi san 2.cpp

---
+++ AtCoder/AtCoder Beginner Contest/374/A - Takahashi san 2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    string s; cin >> s;
+    cout << (s.substr(s.length() - 3, 3) == "san" ? "Yes\n" : "No\n");
+}
