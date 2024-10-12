aba78178c3ef32a4f00de63106c11752fd075b1c
John Watson <moriatyjohnwatson@gmail.com>
2024-10-12 21:54:26 +0700

    Create Tree Diameter.cpp

---
+++ CSES Problem Set/Tree Algorithms/Tree Diameter.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/*
+  John Watson
+ 
+  Mua Code nhu mua Florentino !!
+*/
+ 
+#define faster() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define pb push_back
+ 
+const int maxn = 2e5 + 9;
+ 
+vector<int> adj[maxn];
+vector<int> ver;
+int n;
+ 
+// max nodes;
+int hh[maxn];
+int node;
+void dfs_max(int u, int p = -1)
+{
+	if (hh[u] > hh[node]) node = u;
+	
+	for (int v : adj[u])
+	{
+		if (v == p) continue;
+		
+		hh[v] = hh[u] + 1;
+		dfs_max(v, u);
+	}
+}
+ 
+int main()
+{
+	faster();
+ 
+	cin >> n;
+	for (int i = 1; i < n; i++)
+	{
+		int u, v; cin >> u >> v;
+		adj[u].pb(v);
+		adj[v].pb(u);
+	}
+	
+	dfs_max(1);
+	for (int i = 1; i <= n; i++) hh[i] = 0;
+	dfs_max(node);
+ 
+	cout << hh[node] << '\n'; 
+ 
+	return 0;
+}
