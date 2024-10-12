63ee84e5f3dbb299500a878498483a2257dd0fa7
John Watson <moriatyjohnwatson@gmail.com>
2024-10-12 12:05:37 +0700

    Create A - Contest Start.cpp

---
+++ Codeforces/Codeforces Round 727 (Div. 2)/A - Contest Start.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+ll sum(ll n) { return n * (n + 1) / 2; }
+
+void solve()
+{
+    ll n, x, t; cin >> n >> x >> t;
+
+    ll div = min(t / x, n);
+    cout << div * (n - div) + sum(div - 1) << '\n';
+}
