6c329ca0256b179f78be147220f6cf9410b4c4c8
John Watson <moriatyjohnwatson@gmail.com>
2024-10-13 12:23:32 +0700

    Create A - Vasya and Socks.cpp

---
+++ Codeforces/Codeforces Round 262 (Div. 2)/A - Vasya and Socks.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, m; cin >> n >> m;
+
+    int days = 0;
+    while (1)
+    {
+        if (n == 0)
+        {
+            cout << days << '\n';
+            return;
+        }
+        days++;
+        n--;
+        if (days % m == 0) n++;
+    }
+}
