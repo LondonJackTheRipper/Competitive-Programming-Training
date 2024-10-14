db43f197a53de7ae48aa0e32860e6a571d61ef91
John Watson <moriatyjohnwatson@gmail.com>
2024-10-15 05:34:32 +0700

    Create B - I Hate 1111.cpp

---
+++ Codeforces/Codeforces Round 723 (Div. 2)/B - I Hate 1111.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int x; cin >> x;
+
+    int num = x / 11, rem = x % 11;
+    if (num / 10 >= rem)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
