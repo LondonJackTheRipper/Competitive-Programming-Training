2851c5fb677dc628ae51b7c381b4a3f837f6a865
John Watson <moriatyjohnwatson@gmail.com>
2024-10-15 15:18:14 +0700

    Create C - Vasilije in Cacak.cpp

---
+++ Codeforces/Codeforces Round 900 (Div. 3)/C - Vasilije in Cacak.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+ll segment(ll l, ll r) { return (r + 1) * r / 2 - l * (l - 1) / 2; }
+
+void solve()
+{
+    ll n, k, x; cin >> n >> k >> x;
+
+    ll mn = segment(1, k);
+    ll mx = segment(n - k + 1, n);
+
+    cout << (mn <= x && x <= mx ? "YES\n" : "NO\n");
+}
