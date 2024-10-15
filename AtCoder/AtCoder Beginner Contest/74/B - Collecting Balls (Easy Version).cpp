fd40ecffc2df2f3a480c623f017969020b5ef1dd
John Watson <moriatyjohnwatson@gmail.com>
2024-10-15 13:12:09 +0700

    Create B - Collecting Balls (Easy Version).cpp

---
+++ AtCoder/AtCoder Beginner Contest/74/B - Collecting Balls (Easy Version).cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+
+    int res = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int x; cin >> x;
+        res += min(x, k - x);
+    }
+    cout << 2 * res << '\n';
+}
+
