77139f0fad5630900d73401a72ae7e52687376e2
John Watson <moriatyjohnwatson@gmail.com>
2024-10-16 11:57:35 +0700

    Create B - Team Composition: Programmers and Mathematicians.cpp

---
+++ Codeforces/Codeforces Round 756 (Div. 3)/B - Team Composition: Programmers and Mathematicians.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int a, b; cin >> a >> b;
+
+    if (a > b) swap(a, b);
+    if (3ll * a <= b)
+    {
+        cout << a << '\n';
+        return;
+    }
+
+    cout << (a + b) / 4 << '\n';
+}
