58492bac5a7776ac3875a7147d10ed9c7cab2056
John Watson <moriatyjohnwatson@gmail.com>
2024-10-16 20:08:59 +0700

    Create Greatest Common Divisor.cpp

---
+++ CSAcademy/Greatest Common Divisor.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+int GCD(int a, int b) { return (b == 0 ? a : GCD(b, a % b)); }
+
+void solve()
+{
+    int a, b; cin >> a >> b;
+    
+    cout << GCD(a, b) << '\n';
+}
