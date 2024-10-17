b94a138220b01609782fdca3a2de2f5fe57ac534
John Watson <moriatyjohnwatson@gmail.com>
2024-10-17 12:23:01 +0700

    Create A - Case of the Zeros and Ones.cpp

---
+++ Codeforces/Codeforces Round 310 (Div. 2)/A - Case of the Zeros and Ones.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    string s; cin >> s;
+
+    int cnt0 = 0, cnt1 = 0;
+    for (char c : s) if (c == '0') cnt0++; else cnt1++;
+
+    cout << abs(cnt0 - cnt1) << '\n';
+}
