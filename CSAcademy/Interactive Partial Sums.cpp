6a10365e96d61e7d8bb16571970f0aff63a1fce1
John Watson <moriatyjohnwatson@gmail.com>
2024-10-17 19:55:05 +0700

    Create Interactive Partial Sums.cpp

---
+++ CSAcademy/Interactive Partial Sums.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    
+    ll sum = 0;
+    while (n--)
+    {
+        int a; cin >> a;
+        sum += a;
+        cout << sum << endl;
+    }
+}
