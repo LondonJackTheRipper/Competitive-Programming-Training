a23dd2d9a6c4c8a33cb3290797ecb0e93b3f6800
John Watson <moriatyjohnwatson@gmail.com>
2024-10-17 20:08:41 +0700

    Create Previous Divisors.cpp

---
+++ CSAcademy/Previous Divisors.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    
+    vector<int> v;
+    while (n--)
+    {
+        int a; cin >> a;
+        
+        int res = 0;
+        for (int x : v) 
+            if (a == 0 || (x != 0 && a % x == 0))
+                res++;
+        cout << res << endl;
+        
+        v.push_back(a);
+    }
+}
