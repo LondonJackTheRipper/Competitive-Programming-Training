afa6ba5cd0a8b72e20f55c69d971ae81fb4d706d
John Watson <moriatyjohnwatson@gmail.com>
2024-10-18 14:59:57 +0700

    Create B - 01 Game.cpp

---
+++ Codeforces/Educational Codeforces Round 90 (Rated for Div. 2)/B - 01 Game.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    string s; cin >> s;
+
+    int cnt_0 = 0, cnt_1 = 0;
+    for (char c : s)
+        if (c == '0')
+            cnt_0++;
+        else
+            cnt_1++;
+
+    cout << (min(cnt_0, cnt_1) % 2 == 1 ? "DA\n" : "NET\n");
+}
