78f97fc0a5b64c391b6fff43f0a38605a3765bae
John Watson <moriatyjohnwatson@gmail.com>
2024-10-24 12:42:33 +0700

    Create A - Parallelepiped.cpp

---
+++ Codeforces/Codeforces Round 138 (Div. 2)/A - Parallelepiped.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int a, b, c; cin >> a >> b >> c;
+
+    int x = sqrt(a * b / c), y = sqrt(b * c / a), z = sqrt(a * c / b);
+    cout << 4 * (x + y + z) << '\n';
+}
