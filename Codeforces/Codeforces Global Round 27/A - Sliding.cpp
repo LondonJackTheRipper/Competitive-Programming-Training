ee413bf4510066361fae9b7d0b19baeeb6a2541c
John Watson <moriatyjohnwatson@gmail.com>
2024-10-28 12:38:18 +0700

    Create A - Sliding.cpp

---
+++ Codeforces/Codeforces Global Round 27/A - Sliding.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, m, r, c; cin >> n >> m >> r >> c;
+
+    ll res = 1ll * n * m - 1ll * (r - 1) * m - c - (n - r);
+    int rem = n - r;
+    res += 1ll * rem * m;
+
+    cout << res << '\n';
+}
