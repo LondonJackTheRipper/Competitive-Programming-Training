a51249fa8035cbc67daa87968bd11b2572971bf9
John Watson <moriatyjohnwatson@gmail.com>
2024-10-31 20:57:04 +0700

    Create C - Move Brackets.cpp

---
+++ Codeforces/Codeforces Round 653 (Div. 3)/C - Move Brackets.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define faster() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+#define vll vector<long long>
+#define ii pair<int, int>
+#define ll long long
+#define pb push_back
+
+void input()
+{
+	int n; cin >> n;
+	
+	int res = 0;
+	int cur = 0;
+	
+	for (int i = 0; i < n; i++)
+	{
+		char c; cin >> c;
+		if (c == ')') cur--; else
+			cur++;
+			
+		if (cur < 0)
+		{
+			res++;
+			cur = 0;
+		}
+	}
+	
+	cout << res << '\n';
+}
+
+int main()
+{
+   faster();
+
+	int t; cin >> t;
+	while (t--) input();
+
+	return 0;
+}
