c25e7c3e0fc7fe197d8a4f0caee8e05a4826d322
John Watson <moriatyjohnwatson@gmail.com>
2024-11-04 12:46:49 +0700

    Create A - Quintomania.cpp

---
+++ Codeforces/Codeforces Round 984 (Div. 3)/A - Quintomania.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    bool res = true;
+    for (int i = 1; i < n; i++)
+        {
+            int val = abs(a[i] - a[i - 1]);
+            if (val != 5 && val != 7)
+                res = false;
+        }
+
+    cout << (res == true ? "YES\n" : "NO\n");
+}
