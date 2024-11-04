4d9f26784eb14845c8168616f3a9ef199e63b7d8
John Watson <moriatyjohnwatson@gmail.com>
2024-11-04 12:50:08 +0700

    Create C - Repeating.cpp

---
+++ AtCoder/AtCoder Beginner Contest/378/C - Repeating.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    map<int, int> mp;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x; cin >> x;
+        cout << (mp[x] == 0 ? -1 : mp[x]) << ' ';
+        mp[x] = i + 1;
+    }
+}
