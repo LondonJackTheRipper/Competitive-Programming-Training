0245f7a9e18be5018166c4ab84cd9037a8df2c89
John Watson <moriatyjohnwatson@gmail.com>
2024-11-13 10:40:56 +0700

    Create A - Filling Shapes.cpp

---
+++ Codeforces/Codeforces Round 566 (Div. 2)/A - Filling Shapes.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+
+    if (n % 2 == 1)
+    {
+        cout << 0 << '\n';
+        return;
+    }
+    cout << (1 << (n / 2)) << '\n';
+}
