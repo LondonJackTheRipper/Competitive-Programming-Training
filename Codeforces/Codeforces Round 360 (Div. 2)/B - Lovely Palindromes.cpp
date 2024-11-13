fad5510c2ac79909eb9a167e1b01ad6b63c57ed3
John Watson <moriatyjohnwatson@gmail.com>
2024-11-13 10:37:53 +0700

    Create B - Lovely Palindromes.cpp

---
+++ Codeforces/Codeforces Round 360 (Div. 2)/B - Lovely Palindromes.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    string n; cin >> n;
+
+    cout << n;
+    reverse(n.begin(), n.end());
+    cout << n << '\n';
+}
