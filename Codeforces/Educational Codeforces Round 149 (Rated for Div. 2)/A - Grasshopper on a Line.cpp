4569a103593d15ece557a595f4f7cad1cf2c18a8
John Watson <moriatyjohnwatson@gmail.com>
2024-11-18 20:03:26 +0700

    Create A - Grasshopper on a Line.cpp

---
+++ Codeforces/Educational Codeforces Round 149 (Rated for Div. 2)/A - Grasshopper on a Line.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int x, k; cin >> x >> k;
+
+    if (x % k == 0)
+        cout << 2 << '\n' << 1 << ' ' << x - 1 << '\n';
+    else
+        cout << 1 << '\n' << x << '\n';
+}
+
