9d449bca2cc9e1244e3abc224ecc5554fd8c4898
John Watson <moriatyjohnwatson@gmail.com>
2024-11-18 20:00:17 +0700

    Create B - Queries about less or equal elements.cpp

---
+++ Codeforces/Educational Codeforces Round 2/B - Queries about less or equal elements.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, m; cin >> n >> m;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a + n);
+
+    for (int i = 0; i < m; i++)
+    {
+        int b; cin >> b;
+
+        cout << upper_bound(a, a + n, b) - a << ' ';
+    }
+}
