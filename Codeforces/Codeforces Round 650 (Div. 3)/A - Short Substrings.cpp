d96f511e2fd6e42adddaf4778620d180cb347b3f
John Watson <moriatyjohnwatson@gmail.com>
2024-11-25 10:34:12 +0700

    Create A - Short Substrings.cpp

---
+++ Codeforces/Codeforces Round 650 (Div. 3)/A - Short Substrings.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    string s; cin >> s;
+
+    cout << s[0];
+    for (int i = 1; i < (int)s.length(); i += 2)
+    {
+        if (i == (int)s.length() - 1)
+            break;
+        cout << s[i];
+    }
+    cout << s[(int)s.length() - 1] << '\n';
+}
