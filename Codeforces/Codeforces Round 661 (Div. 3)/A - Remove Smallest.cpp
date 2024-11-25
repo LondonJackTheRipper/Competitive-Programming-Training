ab08f5a927f9a40ad9d4181c0253ec0a32bd237f
John Watson <moriatyjohnwatson@gmail.com>
2024-11-25 10:35:34 +0700

    Create A - Remove Smallest.cpp

---
+++ Codeforces/Codeforces Round 661 (Div. 3)/A - Remove Smallest.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define faster() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+#define vll vector<long long>
+#define ii pair<int, int>
+#define ll long long
+#define pb push_back
+
+void input()
+{
+	int n; cin >> n;
+	vector<int> a(n);
+	for (int &x : a) cin >> x;
+	sort(a.begin(), a.end());
+	
+	for (int i = 1; i < a.size(); i++)
+		if (a[i] - a[i - 1] > 1)
+		{
+			cout << "NO\n";
+			return;
+		}
+	cout << "YES\n";
+}
+
+int main()
+{
+   faster();
+
+	int t; cin >> t;
+	while (t--) input();
+
+	return 0;
+}
