3986dd70b8be43c877566f35177cb15517821ce7
John Watson <moriatyjohnwatson@gmail.com>
2024-11-26 14:29:58 +0700

    Create A - Lunch Rush.cpp

---
+++ Codeforces/Codeforces Round 169 (Div. 2)/A - Lunch Rush.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+
+    int res = -2e9;
+    for (int i = 0; i < n; i++)
+    {
+        int f, t; cin >> f >> t;
+        res = max(res, f - max(0, t - k));
+    }
+
+    cout << res << '\n';
+}
