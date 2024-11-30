179f15deb408f18910ca7f030f82a91181981c2c
John Watson <moriatyjohnwatson@gmail.com>
2024-11-30 20:56:03 +0700

    Create A - Daily Cookie.cpp

---
+++ AtCoder/AtCoder Beginner Contest/382/A - Daily Cookie.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, d; cin >> n >> d;
+    string s; cin >> s;
+    int cnt = 0;
+    for (char c : s) if (c == '@') cnt++;
+
+    cout << n - (cnt - d) << '\n';
+}
