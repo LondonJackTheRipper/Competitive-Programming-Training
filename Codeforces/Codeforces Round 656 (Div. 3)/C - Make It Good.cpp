11158bed2bd8f90aa65a730b204de965500c48bb
John Watson <moriatyjohnwatson@gmail.com>
2024-11-30 21:16:58 +0700

    Create C - Make It Good.cpp

---
+++ Codeforces/Codeforces Round 656 (Div. 3)/C - Make It Good.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    int a[n + 1];
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    int pos = n - 1;
+    while (pos > 0)
+    {
+        if (a[pos + 1] > a[pos]) break;
+        pos--;
+    }
+    while (pos > 0)
+    {
+        if (a[pos + 1] < a[pos]) break;
+        pos--;
+    }
+
+    cout << pos << '\n';
+}
