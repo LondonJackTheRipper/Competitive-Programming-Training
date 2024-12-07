acdbb7912950a0bf66c0fe0309c43ad5c540dd16
John Watson <moriatyjohnwatson@gmail.com>
2024-12-07 22:51:06 +0700

    Create B - AND 0, Sum Big.cpp

---
+++ Codeforces/Codeforces Round 716 (Div. 2)/B - AND 0, Sum Big.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+const int mod = 1e9 + 7;
+
+int mul(int a, int b) { return 1ll * a * b % mod; }
+
+int power(int a, int b)
+{
+    int res = 1;
+    while (b)
+    {
+        if (b & 1)
+            res = mul(res, a);
+        a = mul(a, a);
+        b /= 2;
+    }
+    return res;
+}
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+
+    cout << power(n, k) << '\n';
+}
