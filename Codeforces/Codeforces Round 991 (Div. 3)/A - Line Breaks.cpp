e86378bd9ba92ae48bf3a61f73734a0d0fd71101
John Watson <moriatyjohnwatson@gmail.com>
2024-12-07 22:38:21 +0700

    Create A - Line Breaks.cpp

---
+++ Codeforces/Codeforces Round 991 (Div. 3)/A - Line Breaks.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, m; cin >> n >> m;
+
+    int res = 0, cur = 0;
+    for (int i = 0; i < n; i++)
+    {
+        string s; cin >> s;
+        cur += s.length();
+        if (cur <= m) res++;
+    }
+
+    cout << res << '\n';
+}
