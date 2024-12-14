e9e7e930f217f0d86d55dd18a37c52a02084dbe4
John Watson <moriatyjohnwatson@gmail.com>
2024-12-15 05:50:37 +0700

    Create A - aaaadaa.cpp

---
+++ AtCoder/AtCoder Beginner Contest/384/A - aaaadaa.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    char c1, c2; cin >> c1 >> c2;
+
+    string s; cin >> s;
+    for (char c : s)
+        if (c == c1)
+            cout << c;
+        else
+            cout << c2;
+
+    cout << '\n';
+}
