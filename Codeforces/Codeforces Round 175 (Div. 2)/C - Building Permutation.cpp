93c77e1bb52690617e5acefc4f5a0a8edc3749d9
John Watson <moriatyjohnwatson@gmail.com>
2024-12-15 16:57:02 +0700

    Create C - Building Permutation.cpp

---
+++ Codeforces/Codeforces Round 175 (Div. 2)/C - Building Permutation.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    int a[n + 1];
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    sort(a + 1, a + n + 1);
+    ll res = 0;
+    for (int i = 1; i <= n; i++)
+        res += abs(i - a[i]);
+
+    cout << res << '\n';
+}
