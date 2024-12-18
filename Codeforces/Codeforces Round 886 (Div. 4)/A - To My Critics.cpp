46b04ead8c79f4fba715c7c1b3da64be4de670e1
John Watson <moriatyjohnwatson@gmail.com>
2024-12-18 21:28:06 +0700

    Create A - To My Critics.cpp

---
+++ Codeforces/Codeforces Round 886 (Div. 4)/A - To My Critics.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int a, b, c; cin >> a >> b >> c;
+
+    if (a + b > 9 || b + c > 9 || a + c > 9)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
