f54c8cdf4e5fb35b7cfa9f8e94b10635a9bb2d46
John Watson <moriatyjohnwatson@gmail.com>
2024-12-19 16:10:09 +0700

    Create A - Mishka and Game.cpp

---
+++ Codeforces/Codeforces Round 365 (Div. 2)/A - Mishka and Game.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+
+    int ans1 = 0, ans2 = 0;
+    while (n--)
+    {
+        int a, b; cin >> a >> b;
+
+        if (a > b)
+            ans1++;
+        else
+        if (a < b)
+            ans2++;
+    }
+
+    if (ans1 == ans2)
+        cout << "Friendship is magic!^^";
+    else
+        cout << (ans1 > ans2 ? "Mishka" : "Chris");
+}
