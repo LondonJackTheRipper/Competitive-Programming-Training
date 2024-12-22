3333ae8f5fbb6916112bc5eee0926045293beb93
John Watson <moriatyjohnwatson@gmail.com>
2024-12-22 12:45:34 +0700

    Create B - Just Eat It!.cpp

---
+++ Codeforces/Codeforces Round 613 (Div. 2)/B - Just Eat It!.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    int a[n];
+    ll Yasser = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        Yasser += a[i];
+    }
+
+    ll pre = 0;
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
+    pq.push({0, 0});
+    for (int i = 0; i < n; i++)
+    {
+        pre += a[i];
+
+        if (i == n - 1 && pq.top().second == 0)
+            pq.pop();
+        if (pre - pq.top().first >= Yasser)
+        {
+            cout << "NO\n";
+            return;
+        }
+
+        pq.push({pre, i + 1});
+    }
+
+    cout << "YES\n";
+}
