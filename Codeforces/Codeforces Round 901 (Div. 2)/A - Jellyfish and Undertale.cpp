2745a8c27de2eab949942723a2d45b2a99de8a10
John Watson <moriatyjohnwatson@gmail.com>
2024-12-31 16:31:25 +0700

    Create A - Jellyfish and Undertale.cpp

---
+++ Codeforces/Codeforces Round 901 (Div. 2)/A - Jellyfish and Undertale.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int a, b, n; cin >> a >> b >> n;
+    ll res = b;
+    for (int i = 0; i < n; i++)
+    {
+        int x; cin >> x;
+        res += min(a - 1, x);
+    }
+
+    cout << res << '\n';
+}
