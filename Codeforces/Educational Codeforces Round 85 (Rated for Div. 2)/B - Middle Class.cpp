02f843af27abc60b26abb19e43c083e044819491
John Watson <moriatyjohnwatson@gmail.com>
2025-01-07 20:36:41 +0700

    Create B - Middle Class.cpp

---
+++ Codeforces/Educational Codeforces Round 85 (Rated for Div. 2)/B - Middle Class.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, x; cin >> n >> x;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a, a + n, greater<int>());
+    int res = 0;
+    ll sum = 0;
+    while (res < n && (sum + a[res]) / (res + 1) >= x)
+        sum += a[res++];
+
+    cout << res << '\n';
+}
