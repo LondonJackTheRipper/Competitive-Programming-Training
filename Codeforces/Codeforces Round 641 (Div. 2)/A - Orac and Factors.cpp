185aff1d4108baa22bf497b19a7c26987f24a7db
John Watson <moriatyjohnwatson@gmail.com>
2025-01-07 20:26:36 +0700

    Create A - Orac and Factors.cpp

---
+++ Codeforces/Codeforces Round 641 (Div. 2)/A - Orac and Factors.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+    int mn;
+    for (int i = n; i > 1; i--)
+        if (n % i == 0)
+            mn = i;
+
+    cout << (mn == 2 ? n + 1ll * mn * k : n + mn + 1ll * 2 * (k - 1)) << '\n';
+}
