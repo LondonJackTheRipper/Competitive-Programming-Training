7f77c0fa0168a8662da1882a8567e4bcaff5f159
John Watson <moriatyjohnwatson@gmail.com>
2025-01-12 07:27:12 +0700

    Create C - Balanced Team.cpp

---
+++ Codeforces/Codeforces Round 544 (Div. 3)/C - Balanced Team.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a, a + n);
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int pos = lower_bound(a, a + n, a[i] - 5) - a;
+
+        res = max(res, i - pos + 1);
+    }
+
+    cout << res << '\n';
+}
