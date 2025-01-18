1eecc8554f341ff83e8876424df267571d60f6ff
John Watson <moriatyjohnwatson@gmail.com>
2025-01-18 20:52:35 +0700

    Create B - 9x9 Sum.cpp

---
+++ AtCoder/AtCoder Beginner Contest/387/B - 9x9 Sum.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int x; cin >> x;
+
+    int res = 0;
+    for (int i = 1; i <= 9; i++)
+        for (int j = 1; j <= 9; j++)
+            if (i * j != x)
+                res += i * j;
+
+    cout << res << '\n';
+}
