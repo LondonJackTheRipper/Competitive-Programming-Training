e360e14ae7b90eeacc781a00d3a9729a73404426
John Watson <moriatyjohnwatson@gmail.com>
2025-01-20 12:19:28 +0700

    Create D - Subtract Min Sort.cpp

---
+++ Codeforces/Codeforces Round 998 (Div. 3)/D - Subtract Min Sort.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    int a[n];
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (a[i] < a[i - 1])
+        {
+            cout << "NO\n";
+            return;
+        }
+
+        a[i] -= a[i - 1];
+    }
+
+    cout << "YES\n";
+}
