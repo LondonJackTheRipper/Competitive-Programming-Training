a2ee95a6ed7ec26020bca17d558e448c16d0a7de
John Watson <moriatyjohnwatson@gmail.com>
2025-02-07 17:07:07 +0700

    Create A - Vanya and Cubes.cpp

---
+++ Codeforces/Codeforces Round 280 (Div. 2)/A - Vanya and Cubes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+
+    for (int i = 1; i < (int)1e4; i++)
+    {
+        n -= (i + 1) * i / 2;
+        if (n < 0)
+        {
+            cout << i - 1 << '\n';
+            break;
+        }
+    }
+}
