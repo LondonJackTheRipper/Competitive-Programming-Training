09d259968037d62a5e975837078796725b169aef
Ben Watson <moriatyjohnwatson@gmail.com>
2025-03-02 20:51:53 +0700

    Create B - Array Recovery.cpp

---
+++ Codeforces/Educational Codeforces Round 136 (Rated for Div. 2)/B - Array Recovery.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    int d[n + 1];
+    for (int i = 1; i <= n; i++)
+        cin >> d[i];
+
+    int prefix = d[1];
+    vector<int> ans(1, d[1]);
+    for (int i = 2; i <= n; i++)
+    {
+        if (d[i] != 0 && prefix - d[i] >= 0)
+        {
+            cout << -1 << '\n';
+            return;
+        }
+        prefix += d[i];
+        ans.push_back(prefix);
+    }
+
+    for (int x : ans)
+        cout << x << ' ';
+    cout << '\n';
+}
