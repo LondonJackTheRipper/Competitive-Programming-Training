55fe7de6bdbdf4172a75e19cd7fa6006adf2c62b
Ben Watson <moriatyjohnwatson@gmail.com>
2025-03-02 20:51:42 +0700

    Create A - Split the Multiset.cpp

---
+++ Codeforces/Codeforces Round 958 (Div. 2)/A - Split the Multiset.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+
+    if (n <= k)
+        cout << (n == 1 ? 0 : 1) << '\n';
+    else
+    {
+        int m = (n - 2) / (k - 1);
+
+        cout << m + (n - (k - 1) * m != 0) << '\n';
+    }
+}
