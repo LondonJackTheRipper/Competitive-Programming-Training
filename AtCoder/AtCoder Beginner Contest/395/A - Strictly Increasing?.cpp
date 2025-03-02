f57675c4eab9012385eae79bcf5dd703f3b7570c
John Watson <moriatyjohnwatson@gmail.com>
2025-03-02 09:11:25 +0700

    Create A - Strictly Increasing?.cpp

---
+++ AtCoder/AtCoder Beginner Contest/395/A - Strictly Increasing?.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  John Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+
+    int prev = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x; cin >> x;
+        if (i != 0 && x <= prev)
+        {
+            cout << "No\n";
+            return;
+        }
+        prev = x;
+    }
+    cout << "Yes\n";
+}
