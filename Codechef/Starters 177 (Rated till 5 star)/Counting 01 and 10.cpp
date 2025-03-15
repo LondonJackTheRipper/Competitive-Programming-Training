eed775ecce4476853be67d58d5b654fd4354ec81
Ben Watson <moriatyjohnwatson@gmail.com>
2025-03-15 18:29:46 +0700

    Create Counting 01 and 10.cpp

---
+++ Codechef/Starters 177 (Rated till 5 star)/Counting 01 and 10.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  Ben Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    
+    ll ans = 0;
+    for (int i = 0; i <= n; i++)
+    { 
+        if (n - i < i) 
+            break;
+        ans += 1ll * i * (n - i) + 1;
+    }
+    
+    cout << ans << '\n';
+}
