30a48e26c7c1db21ad280a7362e7f1faa72063c2
Ben Watson <moriatyjohnwatson@gmail.com>
2025-03-27 16:01:53 +0700

    Create Big Difference.cpp

---
+++ Codechef/Starters 178 (Rated till 6 star)/Big Difference.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  Ben Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+    
+    if (n == 1)
+        cout << -1 << ' ' << -1 << '\n';
+    else
+    {
+        if (n % 2 == 0) n--;
+        
+        if (n >= k + 2)
+            cout << 2 << ' ' << n << '\n';
+        else
+            cout << -1 << ' ' << -1 << '\n';
+    }
+}
