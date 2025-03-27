9b4cf795147f010a49c7c02c496efd17589cf1e9
Ben Watson <moriatyjohnwatson@gmail.com>
2025-03-27 16:02:35 +0700

    Create Placing 01 And 10.cpp

---
+++ Codechef/Starters 178 (Rated till 6 star)/Placing 01 And 10.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  Ben Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int x, y; cin >> x >> y;
+    
+    cout << x + y + max(0, abs(x - y) - 1) << '\n'; 
+}
