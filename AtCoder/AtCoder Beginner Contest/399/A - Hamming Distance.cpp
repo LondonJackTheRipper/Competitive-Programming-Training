37b6d909aefa58fbe0a9567781513e7ea50f5a50
Ben Watson <moriatyjohnwatson@gmail.com>
2025-04-12 14:59:31 +0700

    Create A - Hamming Distance.cpp

---
+++ AtCoder/AtCoder Beginner Contest/399/A - Hamming Distance.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  Ben Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+const string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    string s, t; cin >> s >> t;
+
+    int dis = 0;
+    for (int i = 0; i < n; i++)
+        dis += (s[i] != t[i]);
+
+    cout << dis << '\n';
+}
