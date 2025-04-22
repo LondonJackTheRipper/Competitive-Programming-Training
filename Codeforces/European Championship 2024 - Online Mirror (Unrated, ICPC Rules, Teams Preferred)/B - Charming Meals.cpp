18c7919db4c9df2b5ee2472cfc9f1ebe91794434
Ben Watson <moriatyjohnwatson@gmail.com>
2025-04-22 12:05:05 +0700

    Create B - Charming Meals.cpp

---
+++ Codeforces/European Championship 2024 - Online Mirror (Unrated, ICPC Rules, Teams Preferred)/B - Charming Meals.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  Ben Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    int a[n], b[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    sort(a, a + n);
+    sort(b, b + n);
+    int res = 0;
+    for (int t = 0; t < n; t++)
+    {
+        int ans = 2e9;
+        for (int i = 0; i <= t; i++)
+            ans = min(ans, abs(a[i] - b[n - t - 1 + i]));
+        for (int i = t + 1; i < n; i++)
+            ans = min(ans, abs(a[i] - b[i - t - 1]));
+
+        res = max(res, ans);
+    }
+
+    cout << res << '\n';
+}
