98ae87ee2264114f73ca8dfc9910a5c020a163ac
Ben Watson <moriatyjohnwatson@gmail.com>
2025-04-22 11:23:28 +0700

    Create A - Common Multiple.cpp

---
+++ Codeforces/Codeforces Round 1019 (Div. 2)/A - Common Multiple.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  Ben Watson
+  Handle codeforces : quangminh98
+
+  Mua Code nhu mua Florentino !!
+*/
+
+#define ll long long
+
+string name = "test";
+
+void solve();
+signed main()
+{
+    if (fopen((name + ".inp").c_str(), "r"))
+    {
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+// main program
+
+void solve()
+{
+    int n; cin >> n;
+    set<int> se;
+    for (int i = 0; i < n; i++)
+    {
+        int x; cin >> x;
+        se.insert(x);
+    }
+
+    cout << (int)se.size() << '\n';
+}
